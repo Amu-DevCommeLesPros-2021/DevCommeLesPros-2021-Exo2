8b5c2b2a23bc315a7c5882a579393edc43924e01
Thierry Seegers <surfintheusa@yahoo.com>
2021-01-06 17:06:06 +0100

    Remove reference to binary stream functions.

--- lib/Vigenere.c
+++ lib/Vigenere.c
@@ -22,13 +22,3 @@ void dechiffre_Vigenere_flux_texte(FILE* clair, FILE* chiffre, char const* cle)
 {
     return;
 }
-
-void chiffre_Vigenere_flux_binaire(FILE* chiffre, FILE* clair, char const* cle)
-{
-    return;
-}
-
-void dechiffre_Vigenere_flux_binaire(FILE* clair, FILE* chiffre, char const* cle)
-{
-    return;
-}
--- lib/Vigenere.h
+++ lib/Vigenere.h
@@ -14,17 +14,3 @@ char* dechiffre_Vigenere(char* chiffre, char const* cle);
 // L'implémentation et les restrictions sont les mêmes que (de)chiffre_Vigenere.
 void chiffre_Vigenere_flux_texte(FILE* chiffre, FILE* clair, char const* cle);
 void dechiffre_Vigenere_flux_texte(FILE* clair, FILE* chiffre, char const* cle);
-
-// Chiffre et déchiffre un flux binaire vers un autre flux.
-// La clé doit être une chaîne de caractères compris entre 'a' et 'z'.
-// La fonction lit le fichier avec getc et chiffre les caractères reçus un par un en les incrémentant d'une valeur donné par la clé.
-// La fonction suit le même principe d'utilisation de la clé que (de)chiffre_Vigenere.
-// Les caractères doivent être considérés comme de type "unsigned char". De ce fait, si la valeur
-// incrémentée dépasse la valeur maximale d'un "unsigned char" (c.-à-d. 255), on doit utiliser un modulo. (e.g. 252 + 12 -> 9)
-// Exemple:
-// Fichier clair: [0x00, 0x01, 0x02, 0x10, 0xAB, 0xFC]
-// Clé: "fil"
-// Fichier chiffré: [0x05, 0x09, 0x0D, 0x15, 0xB3, 0x09]
-// Explication: [0x00 + 'f' -> 0x05, 0x01 + 'i' -> 0x09, 0x02 + 'l' -> 0x0D, 0x10 + 'f' -> 0x15, 0xAB + 'i' -> 0xB3, 0xFC + 'l' -> 0x09]
-void chiffre_Vigenere_flux_binaire(FILE* chiffre, FILE* clair, char const* cle);
-void dechiffre_Vigenere_flux_binaire(FILE* clair, FILE* chiffre, char const* cle);
